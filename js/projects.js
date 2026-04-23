9ff61e737b5e84b24c0dab0be6504de25ecc79b9
Aadil Khan <65149540+aadilkhan27@users.noreply.github.com>
2026-04-23 10:09:09 +0530

    Revise project list and descriptions in projects.js
    Updated project descriptions and added new projects.

--- js/projects.js
+++ js/projects.js
@@ -1,16 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   const projects = [
-    ["Ambrela Money App","Secure payment system","amberla.webp"],
+    ["Ambrela Money App","All your fintech solutions in a single application","amberla.webp"],
     ["Rosec Jewels","A robust and secure eCommerce platform for every type of jewelry requirement.","rosec.webp"],
     ["Dhava","Dhava – Your Smart Fitness Companion.","dhava.jpg"],
-    ["MooRakshak","Smart dairy system","moorakshak-mobile-app.png"],
-    ["Education App","Offline learning platform","ar-education.webp"],
-    ["Play Sports","Training platform","play-sports.webp"],
-    ["KTPL","Dairy system","kptl-mobile-app.png"],
+    ["Timeout","Time Out is an application that allows you to track your workouts and manage your health.","time-out.webp"],
+    ["Recommend You","Recommend You is an amazing social media app with lots of cool features.","recommend-u.webp"],
+    ["MooRakshak","Protecting cows, empowering farmers and nurturing a planet-positive dairy future","moorakshak-mobile-app.png"],
+    ["AR Education App","AR Education is an excellent application for every student to level up their studies","ar-education.webp"],
+    ["Play Sports","Sports event and training","play-sports.webp"],
+    ["KTPL","KTPL India: Pioneering Dairy Equipment for a Smarter Tomorrow","kptl-mobile-app.png"],
     ["Enertrak","Live energy analytics","enerket.webp"],
-    ["Great11","Real-time scoring system","great11.webp"],
-    ["RosterUp","League platform","rosterup.webp"],
+    ["Great11","Fantasy Sporst mobile application","great11.webp"],
+    ["RosterUp","eSporst fantasy league platform","rosterup.webp"],
     ["Smart-Boss","Building a robust platform to manage construction workers and machinery.","smartboss.webp"]
   ];
 
